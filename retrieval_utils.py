161b96707b3a2895a95eda277f1545375a246eae
ankumar8164 <ankumar8164@gmail.com>
2025-05-14 11:45:28 -0500

    fixed some formatting

--- retrieval_utils.py
+++ retrieval_utils.py
@@ -10,10 +10,9 @@ def load_questions_from_csv(csv_path, question_col = 'Question', pages_col = 'Pa
         pages_raw = str(row.get(pages_col, '')).strip()
         if not q or not pages_raw:
             continue
-        
+
         nums = re.findall(r'\b\d{1,3}\b', pages_raw)
         pages = [int(n) for n in nums if 1 <= int(n) <= max_page]
-
         if pages:
             results.append((q, pages))
 
